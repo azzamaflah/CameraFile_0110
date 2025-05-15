24299b637414c163972692773265b5e3ea733812
azzamaflah <azzamhebat46@gmail.com>
2025-05-15 20:36:29 +0700

    Menambahkan CameraBloc untuk menangani berbagai event kamera seperti inisialisasi, switch kamera, dan pengaturan flash

---
+++ lib/bloc/camera_bloc.dart
@@ -0,0 +1,36 @@
+import 'dart:io';
+import 'package:bloc/bloc.dart';
+import 'package:camera/camera.dart';
+import 'package:camerafile/bloc/camera_event.dart';
+import 'package:camerafile/bloc/camera_state.dart';
+
+class CameraBloc extends Bloc<CameraEvent, CameraState> {
+  late final List<CameraDescription> _cameras;
+
+  CameraBloc() : super(CameraInitial()) {
+    on<InitializeCamera>(_onInit);
+    on<SwitchCamera>(_onSwitch);
+    on<ToogleFlash>(_onToggleFlash);
+    on<TakePicture>(_onTakePicture);
+    on<TapToFocus>(_onTapFocus);
+    on<PickImageFromGallery>(_onPickGallery);
+    on<OpenCameraAndCapture>(_onOpenCamera);
+    on<DeleteImage>(_onDeleteImage);
+    on<ClearSnackbar>(_onClearSnackbar);
+    on<RequestPermission>(_onRequestPermissions);
+  }
+
+  Future<void> _onInit(
+      InitializeCamera event, Emitter<CameraState> emit) async {
+    _cameras = await availableCameras();
+
+    await _setupController(0, emit);
+  }
+
+   Future<void> _onSwitch(SwitchCamera event, Emitter<CameraState> emit) async {
+    if (state is! CameraReady) return;
+    final s = state as CameraReady;
+    final next = (s.selectedIndex + 1) % _cameras.length;
+    await _setupController(next, emit, previous: s);
+  }
+}
